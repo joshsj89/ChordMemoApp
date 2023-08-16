06f3364fac772d339375956ad773422bd3899647
joshsj89 <joshsj89@gmail.com>
2023-08-16 06:18:48 -0700

    Added SocialMediaButtons

--- Navigation.js
+++ Navigation.js
@@ -4,9 +4,27 @@ import { DrawerToggleButton, createDrawerNavigator } from '@react-navigation/dra
 import HomeScreen from './screens/HomeScreen';
 import SongDetailsScreen from './screens/SongDetailsScreen';
 import AddSongScreen from './screens/AddSongScreen';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Image, Linking, View } from 'react-native';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+
+function SocialMediaButtons() {
+    return (
+        <View style={{ flexDirection: 'row' }}>
+            <TouchableOpacity onPress={() => Linking.openURL('https://www.github.com/joshsj89')}>
+                <Image source={require('./assets/images/github-mark-white.png')} style={{ width: 30, height: 30, marginRight: 10 }} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => Linking.openURL('https://joshsj89.github.io')}>
+                <Image source={require('./assets/images/website_logo.png')} style={{ width: 30, height: 30, marginRight: 10 }} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => Linking.openURL('mailto:joshsj89@gmail.com')}>
+                <Image source={require('./assets/images/email.png')} style={{ width: 30, height: 22, marginRight: 10, marginTop: 4 }} />
+            </TouchableOpacity>
+        </View>
+    );
+}
 
 function Navigation() {
     return (
@@ -42,7 +60,8 @@ function HomeStack() {
                     headerTintColor: '#fff',
                     headerLeft: () => (
                         <DrawerToggleButton tintColor="white" onPress={() => navigation.toggleDrawer()} />
-                    )
+                    ),
+                    headerRight: () => <SocialMediaButtons />
                 })} 
             />
             <Stack.Screen 
