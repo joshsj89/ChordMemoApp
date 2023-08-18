996df3be56121705bb3e39c22d004a6d4aba2a63
joshsj89 <joshsj89@gmail.com>
2023-08-18 04:55:01 -0700

    Added Tooltips to SocialMediaButtons

--- Navigation.js
+++ Navigation.js
@@ -5,7 +5,10 @@ import HomeScreen from './screens/HomeScreen';
 import SongDetailsScreen from './screens/SongDetailsScreen';
 import AddSongScreen from './screens/AddSongScreen';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Image, Linking, View } from 'react-native';
+import { Image, Linking, View, Text } from 'react-native';
+import { Tooltip } from 'react-native-paper';
+import { useState } from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -13,62 +16,70 @@ const Drawer = createDrawerNavigator();
 function SocialMediaButtons() {
     return (
         <View style={{ flexDirection: 'row' }}>
-            <TouchableOpacity onPress={() => Linking.openURL('https://www.github.com/joshsj89')}>
-                <Image source={require('./assets/images/github-mark-white.png')} style={{ width: 25, height: 25, marginRight: 10 }} />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => Linking.openURL('https://joshsj89.github.io')}>
-                <Image source={require('./assets/images/website_logo.png')} style={{ width: 25, height: 25, marginRight: 10 }} />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => Linking.openURL('mailto:joshsj89@gmail.com')}>
-                <Image source={require('./assets/images/email.png')} style={{ width: 25, height: 18, marginRight: 10, marginTop: 4 }} />
-            </TouchableOpacity>
+            <Tooltip title ="GitHub" leaveTouchDelay={250} theme={{ colors: { text: 'white', primary: '#009788' } }}>
+                <TouchableOpacity onPress={() => Linking.openURL('https://www.github.com/joshsj89')}>
+                    <Image source={require('./assets/images/github-mark-white.png')} style={{ width: 25, height: 25, marginRight: 10 }} />
+                </TouchableOpacity>
+            </Tooltip>
+            <Tooltip title ="My Website" leaveTouchDelay={250}>
+                <TouchableOpacity onPress={() => Linking.openURL('https://joshsj89.github.io')}>
+                    <Image source={require('./assets/images/website_logo.png')} style={{ width: 25, height: 25, marginRight: 10 }} />
+                </TouchableOpacity>
+            </Tooltip>
+            <Tooltip title ="Email" leaveTouchDelay={250}>
+                <TouchableOpacity onPress={() => Linking.openURL('mailto:joshsj89@gmail.com')}>
+                    <Image source={require('./assets/images/email.png')} style={{ width: 25, height: 18, marginRight: 10, marginTop: 4 }} />
+                </TouchableOpacity>
+            </Tooltip>
         </View>
     );
 }
 
 function Navigation() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName='Home'>
-                <Stack.Screen 
-                    name="Home" 
-                    component={HomeDrawer} 
-                    options={({ navigation }) => ({
-                        title: 'ChordMemo',
-                        headerStyle: {
-                            backgroundColor: '#009788',
-                        },
-                        headerTintColor: '#fff',
-                        headerLeft: () => (
-                            <DrawerToggleButton tintColor="white" onPress={() => navigation.toggleDrawer()} />
-                        ),
-                        headerRight: () => <SocialMediaButtons />
-                    })} 
-                />
-                <Stack.Screen 
-                    name="SongDetails" 
-                    component={SongDetailsScreen} 
-                    options={({ route }) => ({ 
-                        title: route.params.song.title,
-                        headerStyle: {
-                            backgroundColor: '#009788',
-                        },
-                        headerTintColor: '#fff'
-                    })} 
-                />
-                <Stack.Screen 
-                    name="AddSong" 
-                    component={AddSongScreen} 
-                    options={{ 
-                        title: 'Add Song',
-                        headerStyle: {
-                            backgroundColor: '#009788',
-                        },
-                        headerTintColor: '#fff' 
-                    }} 
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <PaperProvider>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName='Home'>
+                    <Stack.Screen 
+                        name="Home" 
+                        component={HomeDrawer} 
+                        options={({ navigation }) => ({
+                            title: 'ChordMemo',
+                            headerStyle: {
+                                backgroundColor: '#009788',
+                            },
+                            headerTintColor: '#fff',
+                            headerLeft: () => (
+                                <DrawerToggleButton tintColor="white" onPress={() => navigation.toggleDrawer()} />
+                                ),
+                            headerRight: () => <SocialMediaButtons />
+                        })} 
+                    />
+                    <Stack.Screen 
+                        name="SongDetails" 
+                        component={SongDetailsScreen} 
+                        options={({ route }) => ({ 
+                            title: route.params.song.title,
+                            headerStyle: {
+                                backgroundColor: '#009788',
+                            },
+                            headerTintColor: '#fff'
+                        })} 
+                        />
+                    <Stack.Screen 
+                        name="AddSong" 
+                        component={AddSongScreen} 
+                        options={{ 
+                            title: 'Add Song',
+                            headerStyle: {
+                                backgroundColor: '#009788',
+                            },
+                            headerTintColor: '#fff' 
+                        }} 
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </PaperProvider>
     );
 }
 
