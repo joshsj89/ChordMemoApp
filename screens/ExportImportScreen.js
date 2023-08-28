b006053a359180c74f35e1991dd68f9176349def
joshsj89 <joshsj89@gmail.com>
2023-08-28 15:59:50 -0400

    Added Export feature

--- screens/ExportImportScreen.js
+++ screens/ExportImportScreen.js
@@ -1,15 +1,20 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useState } from "react";
 import { Button, View } from "react-native";
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as FileSystem from 'expo-file-system';
+import { shareAsync } from "expo-sharing";
 
 function ExportImportScreen() {
     const exportSongs = async () => {
         try {
             const savedSongs = await AsyncStorage.getItem('songs');
-            if (savedSongs != null) {
-                // export savedSongs
+            if (savedSongs != null && savedSongs != '[]') {
+                const fileName = 'songs.json';
+                const filePath = FileSystem.documentDirectory + fileName;
+
+                await FileSystem.writeAsStringAsync(filePath, savedSongs);
+                shareAsync(filePath);
                 alert('Exporting songs...');
+
                 // find if there's a way to encrypt (in a sense)
             } else {
                 //Tell user that there are no songs
@@ -36,8 +41,8 @@ function ExportImportScreen() {
 
     return (
         <View>
-            <Button title="Export" onPress={exportSongs} />
-            <Button title="Import" onPress={importSongs} />
+            <Button title="Export" onPress={exportSongs} color="#009788" />
+            <Button title="Import" onPress={importSongs} color="#009788" />
         </View>
     );
 }
