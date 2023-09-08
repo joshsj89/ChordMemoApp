3fd01643d3d03b7b850efc4ee2a4e1709811dc28
joshsj89 <joshsj89@gmail.com>
2023-09-07 20:34:51 -0700

    Made SplashScreen 3 seconds

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navigation from './Navigation';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 function App() {
+    const [appIsReady, setAppIsReady] = useState(false);
+
+    setTimeout(async () => {
+        await SplashScreen.hideAsync();
+        setAppIsReady(true);
+    }, 3000);
+
+    if (!appIsReady) {
+        return null;
+    }
+
     return <Navigation />;
 }
 
