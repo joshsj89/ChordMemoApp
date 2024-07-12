905052c9269f26ab7ef507b7fecb66a3bfde0e9b
joshsj89 <joshsj89@gmail.com>
2024-07-11 23:26:28 -0700

    Revert "Add working suggestion dropdown to SearchDialog"
    This reverts commit 5b0adcf041f06a25f25f4245f93a48aa170b6a72.

--- components/SearchDialog.tsx
+++ components/SearchDialog.tsx
@@ -1,11 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Modal, Text, View, TextInput, Button } from 'react-native';
 import RadioButtonsGroup from 'react-native-radio-buttons-group';
 import { useTheme } from './ThemeContext';
 import SymbolPickerModal from './SymbolPickerModal';
-import { AutocompleteDropdown, AutocompleteDropdownContextProvider, TAutocompleteDropdownItem } from 'react-native-autocomplete-dropdown';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { title } from 'process';
 
 const radioButtonsData = [
     {
@@ -81,13 +78,6 @@ function SearchDialog({ isVisible, onClose, onSearch }) {
     const [showSearchDialog, setShowSearchDialog] = useState(true);
     const [showSymbolPickerModal, setShowSymbolPickerModal] = useState(false);
 
-    const [suggestions, setSuggestions] = useState<TAutocompleteDropdownItem[]>([]);
-    const [songTitles, setSongTitles] = useState<string[]>([]);
-    const [songArtists, setSongArtists] = useState<string[]>([]);
-    const [songGenres, setSongGenres] = useState<string[]>([]);
-    const [songKeys, setSongKeys] = useState<string[]>([]);
-    const [songChords, setSongChords] = useState<string[]>([]);
-
     const darkMode = useTheme();
 
     const toggleLayer = () => {
@@ -104,149 +94,19 @@ function SearchDialog({ isVisible, onClose, onSearch }) {
         setSearchText('');
     }
 
-    const changeSuggestionList = (searchOptionsVal: string) => {
-        let newSuggestions: TAutocompleteDropdownItem[] = [];
-
-        switch (searchOptionsVal) {
-            case 'title':
-                newSuggestions = songTitles.map((title, index) => {
-                    return {
-                        id: index.toString(),
-                        title: title
-                    };
-                });
-                break;
-            case 'artist':
-                newSuggestions = songArtists.map((artist, index) => {
-                    return {
-                        id: index.toString(),
-                        title: artist
-                    };
-                });
-                break;
-            case 'genre':
-                newSuggestions = songGenres.map((genre, index) => {
-                    return {
-                        id: index.toString(),
-                        title: genre
-                    };
-                });
-                break;
-            case 'key':
-                newSuggestions = songKeys.map((key, index) => {
-                    return {
-                        id: index.toString(),
-                        title: key
-                    };
-                });
-                break;
-            case 'chords':
-                newSuggestions = songChords.map((chords, index) => {
-                    return {
-                        id: index.toString(),
-                        title: chords
-                    };
-                });
-                break;
-            default:
-                break;
-        }
-
-        setSuggestions(newSuggestions);
-    }
-
-    useEffect(() => {
-        const loadSongs = async () => {
-            try {
-                const savedSongs = await AsyncStorage.getItem('songs');
-                if (savedSongs != null) {
-                    const parsedSongs: Song[] = JSON.parse(savedSongs);
-                    // setSongTitles(parsedSongs.map(song => song.title));
-                    // setSongArtists(parsedSongs.map(song => song.artist));
-                    // setSongGenres(parsedSongs.flatMap(song => song.genres));
-                    // setSongKeys(parsedSongs.flatMap(song => song.sections.map(section => section.key.tonic + section.key.symbol + ' ' + section.key.mode)));
-                    // setSongChords(parsedSongs.flatMap(song => song.sections.map(section => section.chords)));
-
-                    const songNames = parsedSongs.map((song, index) => { 
-                        return {
-                            id: index.toString(), 
-                            title: song.title 
-                        };
-                    });
-
-                    const artists = parsedSongs.map((song, index) => {
-                        return {
-                            id: index.toString(),
-                            title: song.artist
-                        };
-                    });
-
-                    const genres = parsedSongs.flatMap(song => song.genres).map((genre, index) => {
-                        return {
-                            id: index.toString(),
-                            title: genre
-                        };
-                    });
-
-                    const keys = parsedSongs.flatMap(song => song.sections.map(section => section.key.tonic + section.key.symbol + ' ' + section.key.mode)).map((key, index) => {
-                        return {
-                            id: index.toString(),
-                            title: key
-                        };
-                    });
-
-                    const chords = parsedSongs.flatMap(song => song.sections.map(section => section.chords)).map((chords, index) => {
-                        return {
-                            id: index.toString(),
-                            title: chords
-                        };
-                    });
-
-                    // no duplicates
-                    const uniqueSongNames = songNames.filter((song, index, self) => self.findIndex(s => s.title === song.title) === index);
-                    const uniqueSongArtists = artists.filter((song, index, self) => self.findIndex(s => s.title === song.title) === index);
-                    const uniqueSongGenres = genres.filter((song, index, self) => self.findIndex(s => s.title === song.title) === index);
-                    const uniqueSongKeys = keys.filter((song, index, self) => self.findIndex(s => s.title === song.title) === index);
-                    const uniqueSongChords = chords.filter((song, index, self) => self.findIndex(s => s.title === song.title) === index);
-
-                    setSongTitles(uniqueSongNames.map(song => song.title));
-                    setSongArtists(uniqueSongArtists.map(artist => artist.title));
-                    setSongGenres(uniqueSongGenres.map(genre => genre.title));
-                    setSongKeys(uniqueSongKeys.map(key => key.title));
-                    setSongChords(uniqueSongChords.map(chords => chords.title));
-                }
-            } catch (error) {
-                console.error('Error loading songs:', error);
-            }
-        }
-
-        loadSongs();
-    }, []);
-
-    useEffect(() => {
-        changeSuggestionList(searchOptions);
-    }, [searchOptions, songTitles])
-
     return (
         <Modal visible={isVisible} transparent={true} animationType='slide' onRequestClose={onClose}>
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)', gap: 30 }}>
                 {showSearchDialog && (
                     <View style={{ backgroundColor: !darkMode ? 'white' : 'black', padding: 20, alignItems: 'center', justifyContent: 'center', borderRadius: 10, borderWidth: !darkMode ? 0 : 1, borderColor: !darkMode ? 'white' : '#444', width: '80%' }}>
                         <Text style={{ fontSize: 20, fontWeight: 'bold', color: !darkMode ? 'black' : 'white' }}>Search</Text>
-                        {/* <TextInput 
+                        <TextInput 
                             placeholder='Enter search text'
                             placeholderTextColor='gray'
                             value={searchText}
                             onChangeText={(text) => setSearchText(text.replace(/#/g, '♯'))}
                             style={{ width: '100%', height: 40, borderColor: !darkMode ? 'gray' : 'white', borderWidth: 1, borderRadius: 5, padding: 5, marginVertical: 10, color: !darkMode ? 'black' : 'white' }}
-                        /> */}
-                        <AutocompleteDropdownContextProvider>
-                            <View style={{ width: '100%', marginVertical: 10 }}>
-                                <AutocompleteDropdown
-                                    dataSet={suggestions}
-                                />
-                            </View>
-                        </AutocompleteDropdownContextProvider>
+                        />
                         <Button
                             title="Insert Symbol"
                             color="#009788"
