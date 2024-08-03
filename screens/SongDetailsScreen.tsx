edd1d1d7786b8ba87c111ff7debc61007871c024
joshsj89 <joshsj89@gmail.com>
2024-08-03 04:18:19 -0600

    Add SongDetailsScreen redesign

--- screens/SongDetailsScreen.tsx
+++ screens/SongDetailsScreen.tsx
@@ -1,4 +1,5 @@
-import { View, Text, Alert, ScrollView } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Text, Alert, ScrollView, TouchableOpacity, StyleSheet, BackHandler } from 'react-native';
 import { FAB } from 'react-native-paper';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
@@ -9,8 +10,28 @@ function SongDetailsScreen({ route }) {
     const { song }: { song: Song } = route.params;
     const { navigate } = useNavigation<HomeScreenNavigationProp>();
     const { goBack } = useNavigation();
-
     const darkMode = useTheme();
+
+    const [showSections, setShowSections] = useState(false);
+
+    const toggleSections = () => {
+        setShowSections(showSections => !showSections);
+    }
+
+    useEffect(() => {
+        const backAction = () => { // close chord keyboard on back button press
+            if (showSections) {
+                setShowSections(false);
+                return true; // prevent default back action
+            } else {
+                return false; // allow default back action
+            }
+        }
+
+        const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+        return () => backHandler.remove(); // cleanup back handler on component unmount
+    }, [showSections]);
 
     const deleteSong = async (songId: string) => {
         try {
@@ -38,19 +59,31 @@ function SongDetailsScreen({ route }) {
     }
 
     return (
-        <View style={{ flex: 1, backgroundColor: !darkMode ? "#F2F2F2" : "black" }}>
-            <ScrollView style={{ padding: 20 }}>
-                <Text style={{ fontSize: 24, fontWeight: 'bold', color: !darkMode ? 'black' : 'white' }}>{song.title}</Text>
-                {song.artist.length > 0 && <Text style={{ fontSize: 16, color: !darkMode ? 'black' : 'white' }}>{song.artist}</Text>}
-                {song.genres.length > 0 && <Text style={{ fontSize: 16, color: !darkMode ? 'black' : 'white' }}>{song.genres.join(', ')}</Text>}
-                <Text style={{ fontSize: 20, fontWeight: 'bold', color: !darkMode ? 'black' : 'white' }}>Sections:</Text>
-                {song.sections.map((section, index) => (
-                    <View key={index}>
-                        <Text style={{ fontSize: 16, fontWeight: 'bold', color: !darkMode ? 'black' : 'white' }}>{section.sectionTitle}</Text>
-                        <Text style={{ fontSize: 16, color: !darkMode ? 'black' : 'white' }}>{section.key.tonic}{section.key.symbol} {section.key.mode} - {section.chords}</Text>
-                    </View>
-                ))}
-            </ScrollView>
+        <View style={[styles.screen, { backgroundColor: !darkMode ? "#F2F2F2" : "black" }]}>
+            <View style={styles.upperContainer}>
+                {showSections && <>
+                    <Text style={{ fontSize: 25, fontWeight: 'bold', color: !darkMode ? 'black' : 'white', textAlign: 'center' }}>{song.title}</Text>
+                    {song.artist.length > 0 && <Text style={{ fontSize: 15, color: !darkMode ? 'black' : 'white', textAlign: 'center' }}>{song.artist}</Text>}
+                </>}
+            </View>
+            <View style={styles.container}>
+                {!showSections && <TouchableOpacity style={[styles.songNameContainer, { borderColor: !darkMode ? 'black' : '#2a2a2a' }]} onPress={toggleSections}>
+                    <Text style={{ fontSize: 50, fontWeight: 'bold', color: !darkMode ? 'black' : 'white', textAlign: 'center' }}>{song.title}</Text>
+                </TouchableOpacity>}
+                {!showSections && <View style={styles.artistContainer}>
+                    {song.artist.length > 0 && <Text style={{ fontSize: 20, color: !darkMode ? 'black' : 'white', textAlign: 'center' }}>{song.artist}</Text>}
+                    {song.genres.length > 0 && <Text style={{ fontSize: 16, color: '#89a1aa', textAlign: 'center' }}>{song.genres.join(', ')}</Text>}
+
+                </View>}
+                {showSections && <ScrollView style={styles.sectionsContainer}>
+                    {song.sections.map((section, index) => (
+                        <View key={index}>
+                            <Text style={{ fontSize: 16, fontWeight: 'bold', color: !darkMode ? 'black' : 'white' }}>{section.sectionTitle}</Text>
+                            <Text style={{ fontSize: 16, color: !darkMode ? 'black' : 'white' }}>{section.key.tonic}{section.key.symbol} {section.key.mode} - {section.chords}</Text>
+                        </View>
+                    ))}
+                </ScrollView>}
+            </View>
             <View style={{ position: 'absolute', right: 5, bottom: 10, flexDirection: 'row', gap: 10 }}>
                 <FAB
                     style={{ backgroundColor: '#009788' }}
@@ -68,5 +101,49 @@ function SongDetailsScreen({ route }) {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1, 
+        paddingTop: 10, 
+        paddingBottom: 100, 
+        paddingHorizontal: 25,
+    },
+    upperContainer: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginBottom: 10,
+        marginHorizontal: 10,
+        minHeight: 56,
+    },
+    container: {
+        margin: 10,
+        borderRadius: 10,
+        flexDirection: 'column',
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    sectionsContainer: { 
+        padding: 10, 
+        width: '100%',
+    },
+    songNameContainer: {
+        padding: 20,
+        borderRadius: 30,
+        width: '100%',
+        height: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+    },
+    artistContainer: {
+        borderRadius: 10,
+        marginTop: 10,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
 
 export default SongDetailsScreen;
