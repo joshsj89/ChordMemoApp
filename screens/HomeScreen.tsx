f936321281597b2a644300dd8c8a43c7e9c2432b
joshsj89 <joshsj89@gmail.com>
2024-08-02 19:15:57 -0600

    Revert "Add back handler to HomeScreen"
    This reverts commit 4660f3fd486fa9a64746417c58fa32a2060932cf.

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity, BackHandler, ToastAndroid } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { FAB } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -9,8 +9,6 @@ import { HomeScreenNavigationProp } from '../types/screens';
 function HomeScreen({ navigation }) {
     const [songs, setSongs] = useState<Song[]>([]);
     const { navigate } = useNavigation<HomeScreenNavigationProp>();
-
-    const [backPressedOnce, setBackPressedOnce] = useState(false);
 
     const darkMode = useTheme();
 
@@ -27,28 +25,11 @@ function HomeScreen({ navigation }) {
             }
         }
 
-        const handleBackPress = () => {
-            if (backPressedOnce) { // If the back button has been pressed once already, exit the app
-                BackHandler.exitApp();
-            } else {
-                setBackPressedOnce(true);
-                ToastAndroid.show('Press back again to exit', ToastAndroid.SHORT);
-
-                setTimeout(() => setBackPressedOnce(false), 4000); // Reset the backPressedOnce state after 2 seconds
-
-                return true; // Prevent default back button behavior
-            }
-        }
-
         const unsubscribe = navigation.addListener('focus', loadSongs); // This is the key to refreshing the list when navigating back to the HomeScreen
-        const backHandler = BackHandler.addEventListener('hardwareBackPress', handleBackPress);
 
-        return () => {
-            unsubscribe();
-            backHandler.remove();
-        };
+        return () => unsubscribe();
         
-    }, [navigation, backPressedOnce]);
+    }, [navigation]);
 
     return (
         <View style={{ flex: 1, backgroundColor: !darkMode ? "#F2F2F2" : "black" }}>
