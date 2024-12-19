6bad9a3bb7dc98bc74e7cd16cff0f5ef7e742ca3
joshsj89 <joshsj89@gmail.com>
2024-12-18 18:26:56 -0800

    Add comments explaining React Context API

--- components/ThemeContext.tsx
+++ components/ThemeContext.tsx
@@ -1,17 +1,25 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { saveDarkModePreference, loadDarkModePreference } from './darkModeHelperFunctions';
 
-const ThemeContext = React.createContext(true);
-const ThemeUpdateContext = React.createContext(() => { });
+// Using React Context API to avoid prop drilling
 
-export function useTheme() {
+
+// Create two contexts: one for the state and one for the function, 
+// allowing for separation of concerns and avoiding unnecessary re-renders
+const ThemeContext = React.createContext(true); // for state
+const ThemeUpdateContext = React.createContext(() => { }); // for function
+
+
+// Custom hooks to use the contexts
+export function useTheme() { // allows for the use of the darkMode state
     return useContext(ThemeContext);
 }
 
-export function useThemeUpdate() {
+export function useThemeUpdate() { // allows for the use of the toggleDarkMode function
     return useContext(ThemeUpdateContext);
 }
 
+// Provider component to wrap the entire app
 export function ThemeProvider({ children }) {
     const [darkMode, setDarkMode] = useState(false);
 
